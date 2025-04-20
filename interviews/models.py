db62b91d8e59a4a9c06ce269b79bd7fc3cefed48
KWY <nivekyang12014@gmail.com>
2025-04-20 23:45:46 +0800

    feat: build models and forms

--- interviews/models.py
+++ interviews/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Interview(models.Model):
+    company_name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    interviewed_at = models.DateField(null=True)
+    rating = models.PositiveSmallIntegerField()
+    review = models.TextField()
+    result = models.CharField()
+
+    
+    
