2c64b0b3d0fd9d533c21db24f20977846b897107
KWY <nivekyang12014@gmail.com>
2025-04-21 21:29:43 +0800

    finish interviews app CRUD challenge

---
+++ interviews/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path 
+from . import views
+
+app_name = "interviews"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("add", views.add, name="add"),
+    path("<int:id>", views.show, name="show"),
+    path("<int:id>/update", views.update, name="update"),
+    path("<int:id>/delete", views.delete, name="delete"),
+]
--- interviews/views.py
+++ interviews/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import InterviewForm
+from .models import Interview
 
 # Create your views here.
+def index(req):
+
+    interviews = Interview.objects.order_by("-id")
+    return render(req, "interviews/index.html", {"interviews": interviews})
+
+def add(req):
+    
+    if req.method == "POST":
+        form = InterviewForm(req.POST)
+        interview = form.save()
+        
+        return redirect("interviews:show", id=interview.id)
+    
+    form = InterviewForm
+    return render(req, "interviews/add.html", {"form": form})
+
+def show(req, id):
+    interview = get_object_or_404(Interview, pk=id)
+    return render(req, "interviews/show.html", {"interview":interview})
+
+def update(req, id):
+    interview = get_object_or_404(Interview, pk=id)
+    if req.method == "POST":
+        form = InterviewForm(req.POST, instance=interview)
+        interview = form.save()
+        return redirect("interviews:show", id=id)
+    
+    form = InterviewForm(instance=interview)
+    
+    return render(req, "interviews/update.html", {"form": form, "id": id})
+
+def delete(req, id):
+    interview = get_object_or_404(Interview, pk=id)
+    interview.delete()
+
+    return redirect("interviews:index")
+
