ec25e61a2c93a767295afc5f179013c6f3b18612
KWY <nivekyang12014@gmail.com>
2025-04-22 02:06:06 +0800

    feat: finish interviews app CRUD implements

--- interviews/models.py
+++ interviews/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Interview(models.Model):
+    company_name = models.CharField(max_length=100)
+    position = models.CharField(max_length=50)
+    interviewed_at = models.DateField(null=True)
+    rating = models.PositiveSmallIntegerField(help_text="請輸入1~10")
+    review = models.TextField()
+    result = models.CharField(max_length=100)
+    
--- interviews/views.py
+++ interviews/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Interview
+from .forms import InterviewForm
 
 # Create your views here.
+def index(req):
+    interviews = Interview.objects.order_by("-id")
+    
+    return render(req, "interviews/index.html", {"interviews": interviews})
+
+def add(req):
+    if req.method == "POST":
+        form = InterviewForm(req.POST)
+        interview = form.save()
+
+        return redirect("interviews:show", id=interview.id)
+
+    form = InterviewForm
+    return render(req, "interviews/add.html", {"form": form}) 
+
+def show(req, id):
+    interview = get_object_or_404(Interview, pk=id)
+    
+    return render(req, "interviews/show.html", {"interview": interview })
+
+def update(req, id):
+    interview = get_object_or_404(Interview, pk=id)
+    if req.method == "POST":
+        form = InterviewForm(req.POST, instance=interview)
+        form.save()
+
+        return redirect("interviews:show", id=id)
+
+    form = InterviewForm(instance=interview)
+    return render(req, "interviews/update.html", {"form": form, "id": id})
+
+def delete(req, id):
+    interview = get_object_or_404(Interview, pk=id)
+    interview.delete()
+
+    return redirect("interviews:index")
+
+
+        
