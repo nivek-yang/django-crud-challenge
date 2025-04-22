7340aa2f4630d7f830b971c7ae303c1af4c379bb
KWY <nivekyang12014@gmail.com>
2025-04-22 11:02:37 +0800

    finish create

--- interviews/views.py
+++ interviews/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Interview
+from .forms import InterviewForm
 
 # Create your views here.
+def add(req):
+    if req.method == "POST":
+        form = InterviewForm(req.POST)
+        interview = form.save()
+        return redirect("interviews:show", id = interview.id)
+
+    form = InterviewForm
+    return render(req, "interviews/add.html", {"form": form})
