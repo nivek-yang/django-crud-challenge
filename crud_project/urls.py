499c7220016225d91cf0b0e78fa31a158382fbd6
KWY <nivekyang12014@gmail.com>
2025-04-22 10:49:45 +0800

    finish models

--- crud_project/urls.py
+++ crud_project/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("pages.urls")),
-    path('', include("interviews.urls"))
+    # path('', include("pages.urls")),
+    # path('', include("interviews.urls"))
 ]
